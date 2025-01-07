deac0030400eb2728f5a589ca96cacf97e7b128b
Marc Jenald Galino <galinomarcjenald20@gmail.com>
2025-01-07 13:48:47 +0800

    divide dashboard layout into components

---
+++ resources/views/components/app/sidebar.blade.php
@@ -0,0 +1,113 @@
+<div x-data="{ sidebarIsOpen: false }" class="relative flex w-full flex-col md:flex-row">
+    <!-- This allows screen readers to skip the sidebar and go directly to the main content. -->
+    <a class="sr-only" href="#main-content">skip to the main content</a>
+
+    <!-- dark overlay for when the sidebar is open on smaller screens  -->
+    <div x-cloak x-show="sidebarIsOpen" class="fixed inset-0 z-20 bg-gray-900/10 backdrop-blur-sm md:hidden"
+        aria-hidden="true" x-on:click="sidebarIsOpen = false" x-transition.opacity></div>
+
+    <nav x-cloak
+        class="fixed left-0 z-30 flex h-svh w-60 shrink-0 flex-col border-r border-gray-500 bg-gray-200 p-4 transition-transform duration-300 md:w-64 md:translate-x-0 md:relative dark:border-gray-500 dark:bg-gray-800"
+        x-bind:class="sidebarIsOpen ? 'translate-x-0' : '-translate-x-60'" aria-label="sidebar navigation">
+        <!-- logo  -->
+        <x-app.logo />
+
+        <!-- search  -->
+        <div class="relative my-4 flex w-full max-w-xs flex-col gap-1 text-gray-800 dark:text-gray-300">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none"
+                stroke-width="2"
+                class="absolute left-2 top-1/2 size-5 -translate-y-1/2 text-gray-800/50 dark:text-gray-300/50"
+                aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+            <input type="search"
+                class="w-full border border-gray-500 rounded-lg bg-gray-50 px-2 py-1.5 pl-9 text-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-900 disabled:cursor-not-allowed disabled:opacity-75 dark:border-gray-500 dark:bg-gray-900/50 dark:focus-visible:outline-sky-400"
+                name="search" aria-label="Search" placeholder="Search" />
+        </div>
+
+        <!-- sidebar links  -->
+        <div class="flex flex-col gap-2 overflow-y-auto pb-6">
+
+            <a wire:navigate href="/dashboard"
+                class="flex items-center rounded-lg gap-2 px-2 py-1.5 text-sm font-medium text-gray-800 underline-offset-2 hover:bg-sky-900/5 hover:text-gray-950 focus-visible:underline focus:outline-none dark:text-gray-300 dark:hover:bg-sky-400/5 dark:hover:text-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 shrink-0"
+                    aria-hidden="true">
+                    <path
+                        d="M15.5 2A1.5 1.5 0 0 0 14 3.5v13a1.5 1.5 0 0 0 1.5 1.5h1a1.5 1.5 0 0 0 1.5-1.5v-13A1.5 1.5 0 0 0 16.5 2h-1ZM9.5 6A1.5 1.5 0 0 0 8 7.5v9A1.5 1.5 0 0 0 9.5 18h1a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 10.5 6h-1ZM3.5 10A1.5 1.5 0 0 0 2 11.5v5A1.5 1.5 0 0 0 3.5 18h1A1.5 1.5 0 0 0 6 16.5v-5A1.5 1.5 0 0 0 4.5 10h-1Z" />
+                </svg>
+                <span>Dashboard</span>
+            </a>
+
+            <a href="#"
+                class="flex items-center rounded-lg gap-2 bg-sky-900/10 px-2 py-1.5 text-sm font-medium text-gray-950 underline-offset-2 focus-visible:underline focus:outline-none dark:bg-sky-400/10 dark:text-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg viewBox="0 0 20 20" fill="currentColor" class="size-5 shrink-0"
+                    aria-hidden="true">
+                    <path
+                        d="M13.92 3.845a19.362 19.362 0 0 1-6.3 1.98C6.765 5.942 5.89 6 5 6a4 4 0 0 0-.504 7.969 15.97 15.97 0 0 0 1.271 3.34c.397.771 1.342 1 2.05.59l.867-.5c.726-.419.94-1.32.588-2.02-.166-.331-.315-.666-.448-1.004 1.8.357 3.511.963 5.096 1.78A17.964 17.964 0 0 0 15 10c0-2.162-.381-4.235-1.08-6.155ZM15.243 3.097A19.456 19.456 0 0 1 16.5 10c0 2.43-.445 4.758-1.257 6.904l-.03.077a.75.75 0 0 0 1.401.537 20.903 20.903 0 0 0 1.312-5.745 2 2 0 0 0 0-3.546 20.902 20.902 0 0 0-1.312-5.745.75.75 0 0 0-1.4.537l.029.078Z" />
+                </svg>
+                <span>Marketing</span>
+                <span class="sr-only">active</span>
+            </a>
+
+            <a href="#"
+                class="flex items-center rounded-lg gap-2 px-2 py-1.5 text-sm font-medium text-gray-800 underline-offset-2 hover:bg-sky-900/5 hover:text-gray-950 focus-visible:underline focus:outline-none dark:text-gray-300 dark:hover:bg-sky-400/5 dark:hover:text-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 shrink-0"
+                    aria-hidden="true">
+                    <path fill-rule="evenodd"
+                        d="M4.606 12.97a.75.75 0 0 1-.134 1.051 2.494 2.494 0 0 0-.93 2.437 2.494 2.494 0 0 0 2.437-.93.75.75 0 1 1 1.186.918 3.995 3.995 0 0 1-4.482 1.332.75.75 0 0 1-.461-.461 3.994 3.994 0 0 1 1.332-4.482.75.75 0 0 1 1.052.134Z"
+                        clip-rule="evenodd" />
+                    <path fill-rule="evenodd"
+                        d="M5.752 12A13.07 13.07 0 0 0 8 14.248v4.002c0 .414.336.75.75.75a5 5 0 0 0 4.797-6.414 12.984 12.984 0 0 0 5.45-10.848.75.75 0 0 0-.735-.735 12.984 12.984 0 0 0-10.849 5.45A5 5 0 0 0 1 11.25c.001.414.337.75.751.75h4.002ZM13 9a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span>Sales</span>
+            </a>
+
+            <a href="#"
+                class="flex items-center rounded-lg gap-2 px-2 py-1.5 text-sm font-medium text-gray-800 underline-offset-2 hover:bg-sky-900/5 hover:text-gray-950 focus-visible:underline focus:outline-none dark:text-gray-300 dark:hover:bg-sky-400/5 dark:hover:text-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 shrink-0"
+                    aria-hidden="true">
+                    <path fill-rule="evenodd"
+                        d="M1 2.75A.75.75 0 0 1 1.75 2h16.5a.75.75 0 0 1 0 1.5H18v8.75A2.75 2.75 0 0 1 15.25 15h-1.072l.798 3.06a.75.75 0 0 1-1.452.38L13.41 18H6.59l-.114.44a.75.75 0 0 1-1.452-.38L5.823 15H4.75A2.75 2.75 0 0 1 2 12.25V3.5h-.25A.75.75 0 0 1 1 2.75ZM7.373 15l-.391 1.5h6.037l-.392-1.5H7.373Zm7.49-8.931a.75.75 0 0 1-.175 1.046 19.326 19.326 0 0 0-3.398 3.098.75.75 0 0 1-1.097.04L8.5 8.561l-2.22 2.22A.75.75 0 1 1 5.22 9.72l2.75-2.75a.75.75 0 0 1 1.06 0l1.664 1.663a20.786 20.786 0 0 1 3.122-2.74.75.75 0 0 1 1.046.176Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span>Performance</span>
+            </a>
+
+            <a href="#"
+                class="flex items-center rounded-lg gap-2 px-2 py-1.5 text-sm font-medium text-gray-800 underline-offset-2 hover:bg-sky-900/5 hover:text-gray-950 focus-visible:underline focus:outline-none dark:text-gray-300 dark:hover:bg-sky-400/5 dark:hover:text-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 shrink-0"
+                    aria-hidden="true">
+                    <path
+                        d="M10 9a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM6 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM1.49 15.326a.78.78 0 0 1-.358-.442 3 3 0 0 1 4.308-3.516 6.484 6.484 0 0 0-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 0 1-2.07-.655ZM16.44 15.98a4.97 4.97 0 0 0 2.07-.654.78.78 0 0 0 .357-.442 3 3 0 0 0-4.308-3.517 6.484 6.484 0 0 1 1.907 3.96 2.32 2.32 0 0 1-.026.654ZM18 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0ZM5.304 16.19a.844.844 0 0 1-.277-.71 5 5 0 0 1 9.947 0 .843.843 0 0 1-.277.71A6.975 6.975 0 0 1 10 18a6.974 6.974 0 0 1-4.696-1.81Z" />
+                </svg>
+                <span>Referrals</span>
+            </a>
+
+            <a href="#"
+                class="flex items-center rounded-lg gap-2 px-2 py-1.5 text-sm font-medium text-gray-800 underline-offset-2 hover:bg-sky-900/5 hover:text-gray-950 focus-visible:underline focus:outline-none dark:text-gray-300 dark:hover:bg-sky-400/5 dark:hover:text-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 shrink-0"
+                    aria-hidden="true">
+                    <path fill-rule="evenodd"
+                        d="M1 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v8a3 3 0 0 1-3 3H4a3 3 0 0 1-3-3V6Zm4 1.5a2 2 0 1 1 4 0 2 2 0 0 1-4 0Zm2 3a4 4 0 0 0-3.665 2.395.75.75 0 0 0 .416 1A8.98 8.98 0 0 0 7 14.5a8.98 8.98 0 0 0 3.249-.604.75.75 0 0 0 .416-1.001A4.001 4.001 0 0 0 7 10.5Zm5-3.75a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Zm0 6.5a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Zm.75-4a.75.75 0 0 0 0 1.5h2.5a.75.75 0 0 0 0-1.5h-2.5Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span>Licenses</span>
+            </a>
+
+            <a href="#"
+                class="flex items-center rounded-lg gap-2 px-2 py-1.5 text-sm font-medium text-gray-800 underline-offset-2 hover:bg-sky-900/5 hover:text-gray-950 focus-visible:underline focus:outline-none dark:text-gray-300 dark:hover:bg-sky-400/5 dark:hover:text-gray-100">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-5 shrink-0"
+                    aria-hidden="true">
+                    <path fill-rule="evenodd"
+                        d="M7.84 1.804A1 1 0 0 1 8.82 1h2.36a1 1 0 0 1 .98.804l.331 1.652a6.993 6.993 0 0 1 1.929 1.115l1.598-.54a1 1 0 0 1 1.186.447l1.18 2.044a1 1 0 0 1-.205 1.251l-1.267 1.113a7.047 7.047 0 0 1 0 2.228l1.267 1.113a1 1 0 0 1 .206 1.25l-1.18 2.045a1 1 0 0 1-1.187.447l-1.598-.54a6.993 6.993 0 0 1-1.929 1.115l-.33 1.652a1 1 0 0 1-.98.804H8.82a1 1 0 0 1-.98-.804l-.331-1.652a6.993 6.993 0 0 1-1.929-1.115l-1.598.54a1 1 0 0 1-1.186-.447l-1.18-2.044a1 1 0 0 1 .205-1.251l1.267-1.114a7.05 7.05 0 0 1 0-2.227L1.821 7.773a1 1 0 0 1-.206-1.25l1.18-2.045a1 1 0 0 1 1.187-.447l1.598.54A6.992 6.992 0 0 1 7.51 3.456l.33-1.652ZM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                        clip-rule="evenodd" />
+                </svg>
+                <span>Settings</span>
+            </a>
+        </div>
+    </nav>
+
+    {{ $slot }}
+</div>
