669064499b4fb8fd2dea60ec1b426d19268373d5
Marc Jenald Galino <galinomarcjenald20@gmail.com>
2025-01-07 13:16:43 +0800

    initial dashboard layout

---
+++ resources/views/livewire/pages/dashboard/overview.blade.php
@@ -0,0 +1,12 @@
+<?php
+
+use Livewire\Volt\Component;
+use Livewire\Attributes\Layout;
+
+new #[Layout('layouts.dashboard')] class extends Component {
+    //
+}; ?>
+
+<div>
+    <h1 class="text-white">This is an overview page</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Volt::route('dashboard', 'pages.dashboard.overview')
+        ->name('dashboard');
+});
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
