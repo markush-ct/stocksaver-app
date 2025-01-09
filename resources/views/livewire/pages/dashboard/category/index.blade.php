1cd0a968aef4aedb0e93cf7cc09221f8f74ecdfc
Marc Jenald Galino <galinomarcjenald20@gmail.com>
2025-01-09 11:53:04 +0800

    add records counter when table row selected

--- resources/views/livewire/pages/dashboard/category/index.blade.php
+++ resources/views/livewire/pages/dashboard/category/index.blade.php
@@ -141,14 +141,22 @@ new #[Layout('layouts.dashboard')] class extends Component
         </div>
     </div>
 
-    <div x-data="{ checkAll : false }" class="overflow-hidden w-full overflow-x-auto rounded-lg border border-gray-500 dark:border-gray-500">
+    <div x-data="{ selectedRowsCount : 0, show : false }" x-show="show" x-on:rows-deselected-all="show = false" x-on:rows-selected.window="selectedRowsCount = $event.detail.selectedRowsCount; show = selectedRowsCount > 0" class="flex justify-between mb-4">
+        <p class="text-gray-800 dark:text-gray-300"><span x-text="selectedRowsCount"></span> records selected</p>
+        <button x-on:click="$dispatch('rows-deselected-all')" class="hover:underline text-red-500">
+            Deselect all
+        </button>
+    </div>
+
+    <div x-data="{ checkAll : false, selectedRowsCount : 0 }" x-on:rows-deselected-all.window="checkAll = false; selectedRowsCount = 0;" class="overflow-hidden w-full overflow-x-auto rounded-lg border border-gray-500 dark:border-gray-500">
         <table class="w-full text-left text-sm text-gray-800 dark:text-gray-300">
             <thead class="border-b border-gray-500 bg-gray-200 text-sm text-gray-950 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-100">
                 <tr>
                     <th scope="col" class="p-4">
                         <label for="checkAll" class="flex items-center cursor-pointer text-gray-800 dark:text-gray-300 ">
                             <div class="relative flex items-center">
-                                <input type="checkbox" x-model="checkAll" id="checkAll" class="before:content[''] peer relative size-4 cursor-pointer appearance-none overflow-hidden rounded border border-gray-500 bg-gray-50 before:absolute before:inset-0 checked:border-sky-900 checked:before:bg-sky-900 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-gray-900 checked:focus:outline-sky-900 active:outline-offset-0 dark:border-gray-500 dark:bg-gray-800 dark:checked:border-sky-400 dark:checked:before:bg-sky-400 dark:focus:outline-gray-300 dark:checked:focus:outline-sky-400" />
+                                <!-- selecting table rows -->
+                                <input x-on:click="$event.target.checked ? selectedRowsCount = {{ $categories->count() }} : selectedRowsCount = 0; $dispatch('rows-selected', { selectedRowsCount : selectedRowsCount })" type="checkbox" x-model="checkAll" id="checkAll" class="before:content[''] peer relative size-4 cursor-pointer appearance-none overflow-hidden rounded border border-gray-500 bg-gray-50 before:absolute before:inset-0 checked:border-sky-900 checked:before:bg-sky-900 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-gray-900 checked:focus:outline-sky-900 active:outline-offset-0 dark:border-gray-500 dark:bg-gray-800 dark:checked:border-sky-400 dark:checked:before:bg-sky-400 dark:focus:outline-gray-300 dark:checked:focus:outline-sky-400" />
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" stroke="currentColor" fill="none" stroke-width="4" class="pointer-events-none invisible absolute left-1/2 top-1/2 size-3 -translate-x-1/2 -translate-y-1/2 text-white peer-checked:visible dark:text-black">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
                                 </svg>
@@ -166,7 +174,8 @@ new #[Layout('layouts.dashboard')] class extends Component
                         <td class="p-4">
                             <label for="{{ $category->id }}" class="flex items-center cursor-pointer text-gray-800 dark:text-gray-300 ">
                                 <div class="relative flex items-center">
-                                    <input type="checkbox" id="{{ $category->id }}" class="before:content[''] peer relative size-4 cursor-pointer appearance-none overflow-hidden rounded border border-gray-500 bg-gray-50 before:absolute before:inset-0 checked:border-sky-900 checked:before:bg-sky-900 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-gray-900 checked:focus:outline-sky-900 active:outline-offset-0 dark:border-gray-500 dark:bg-gray-800 dark:checked:border-sky-400 dark:checked:before:bg-sky-400 dark:focus:outline-gray-300 dark:checked:focus:outline-sky-400" :checked="checkAll" />
+                                    <!-- selecting individual row -->
+                                    <input x-on:click="$event.target.checked ? selectedRowsCount++ : selectedRowsCount--; $dispatch('rows-selected', {selectedRowsCount : selectedRowsCount})" type="checkbox" id="{{ $category->id }}" class="before:content[''] peer relative size-4 cursor-pointer appearance-none overflow-hidden rounded border border-gray-500 bg-gray-50 before:absolute before:inset-0 checked:border-sky-900 checked:before:bg-sky-900 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-gray-900 checked:focus:outline-sky-900 active:outline-offset-0 dark:border-gray-500 dark:bg-gray-800 dark:checked:border-sky-400 dark:checked:before:bg-sky-400 dark:focus:outline-gray-300 dark:checked:focus:outline-sky-400" :checked="checkAll" />
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" stroke="currentColor" fill="none" stroke-width="4" class="pointer-events-none invisible absolute left-1/2 top-1/2 size-3 -translate-x-1/2 -translate-y-1/2 text-white peer-checked:visible dark:text-black">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"/>
                                     </svg>
